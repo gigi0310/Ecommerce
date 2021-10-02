0acb67c86c01c3b90409f5950eea227beca30038
Karen cai <gigi.cai0310@gmail.com>
2021-10-03 00:10:43 +1000

    animation slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/6214479/pexels-photo-6214479.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    // bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/6214384/pexels-photo-6214384.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "POPULAR COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    // bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/6214158/pexels-photo-6214158.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "SEASON SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    // bg: "fbf0f4",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Spanner from '../components/Spanner'
 import Navbar from '../components/Navbar'
-
+import Slider from '../components/Slider'
 const Home = () => {
     return (
         <div>
+          <Spanner />
           <Navbar />
+          <Slider />
         </div>
     )
 }
